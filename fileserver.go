ef583a8a4d16964a59672a55a161c689e86bedc6
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2024-12-01 14:39:59 +0800

    Update fileserver.go

--- fileserver.go
+++ fileserver.go
@@ -7,18 +7,23 @@ import (
 
 // fileServer is a wrapper around http.FileServer that won't do directory listing
 func fileServer(fsys fs.FS) http.Handler {
-	return http.FileServer(wrapperfs{http.FS(fsys)})
+	return http.FileServer(&wrapperfs{http.FS(fsys)})
 }
 
 type wrapperfs struct {
 	fsys http.FileSystem
 }
 
-func (w wrapperfs) Open(name string) (http.File, error) {
+func (w *wrapperfs) Open(name string) (http.File, error) {
 	file, err := w.fsys.Open(name)
 	if err != nil {
 		return nil, err
 	}
+	defer func() {
+		if err != nil {
+			file.Close()
+		}
+	}()
 
 	info, err := file.Stat()
 	if err != nil {
