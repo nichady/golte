d680a95d4b33b78d6e06c5c4e1340f800657d063
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2024-12-01 12:02:05 +0800

    Update renderer.go

--- render/renderer.go
+++ render/renderer.go
@@ -97,8 +97,9 @@ func (r *Renderer) Render(w http.ResponseWriter, data RenderData, csr bool) erro
 	}
 
 	resp.ErrPage = &responseEntry{
-		File: r.renderfile.Manifest[data.ErrPage].Client,
-		CSS:  r.renderfile.Manifest[data.ErrPage].CSS,
+		File:  r.renderfile.Manifest[data.ErrPage].Client,
+		CSS:   r.renderfile.Manifest[data.ErrPage].CSS,
+		Props: map[string]any{},
 	}
 
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
