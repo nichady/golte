e113d35f834ce0c3437d8e66e16679140bf966ee
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2024-12-03 00:33:29 +0800

    update

--- testdata/src/layout/2.svelte
+++ testdata/src/layout/2.svelte
@@ -1,11 +1,11 @@
 <script>
-    import { preload } from "../../../js/public/index.js"
+    // import { preload } from "../../../js/public/index.js"
 </script>
 
 <div id="layout2">
-    <a href="/route1" use:preload={"mount"}>route1</a>
+    <!-- <a href="/route1" use:preload={"mount"}>route1</a>
     <a href="/route2" use:preload={"hover"}>route2</a>
-    <a href="/route3" use:preload={"tap"}>route3</a>
+    <a href="/route3" use:preload={"tap"}>route3</a> -->
 
     <slot/>
 </div>
